d88878aa9b6768583cdcff0eb60a56558dda9560
goutham srikar <srikarsam2000@gmail.com>
2022-07-14 23:22:10 +0530

    naan 2.0 typography

--- lib/utils/styles/styles.dart
+++ lib/utils/styles/styles.dart
@@ -2,6 +2,154 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:naan_wallet/utils/colors/colors.dart';
 
+//* new typo
+
+//? DISPLAY TYPO
+
+TextStyle displayLarge = TextStyle(
+  color: Color.fromRGBO(45, 0, 79, 1),
+  fontFamily: 'Space Grotesk',
+  fontSize: 57,
+  letterSpacing: -0.25,
+  fontWeight: FontWeight.normal,
+  height: 64 / 57,
+);
+
+TextStyle displayMedium = TextStyle(
+  color: Color.fromRGBO(45, 0, 79, 1),
+  fontFamily: 'Space Grotesk',
+  fontSize: 40,
+  letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+  fontWeight: FontWeight.normal,
+  height: 48 / 40,
+);
+
+TextStyle displaySmall = TextStyle(
+  color: Color.fromRGBO(45, 0, 79, 1),
+  fontFamily: 'Space Grotesk',
+  fontSize: 36,
+  letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+  fontWeight: FontWeight.normal,
+  height: 44 / 36,
+);
+
+//? HEADLINE TYPO
+
+TextStyle headlineLarge = TextStyle(
+  color: Color.fromRGBO(45, 0, 79, 1),
+  fontFamily: 'Poppins',
+  fontSize: 32,
+  letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+  fontWeight: FontWeight.normal,
+  height: 40 / 32,
+);
+
+TextStyle headlineMedium = TextStyle(
+  color: Color.fromRGBO(45, 0, 79, 1),
+  fontFamily: 'Poppins',
+  fontSize: 28,
+  letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+  fontWeight: FontWeight.normal,
+  height: 36 / 28,
+);
+
+TextStyle headlineSmall = TextStyle(
+  color: Color.fromRGBO(45, 0, 79, 1),
+  fontFamily: 'Poppins',
+  fontSize: 24,
+  letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+  fontWeight: FontWeight.normal,
+  height: 32 / 24,
+);
+
+//? TITLE TYPO
+
+TextStyle titleLarge = TextStyle(
+  color: Color.fromRGBO(45, 0, 79, 1),
+  fontFamily: 'Poppins',
+  fontSize: 22,
+  letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+  fontWeight: FontWeight.normal,
+  height: 28 / 22,
+);
+
+TextStyle titleMedium = TextStyle(
+  color: Color.fromRGBO(45, 0, 79, 1),
+  fontFamily: 'Poppins',
+  fontSize: 16,
+  letterSpacing: 0.15,
+  fontWeight: FontWeight.normal,
+  height: 24 / 16,
+);
+
+TextStyle titleSmall = TextStyle(
+  color: Color.fromRGBO(45, 0, 79, 1),
+  fontFamily: 'Poppins',
+  fontSize: 14,
+  letterSpacing: 0.1,
+  fontWeight: FontWeight.normal,
+  height: 20 / 14,
+);
+
+//? BODY LARGE
+
+TextStyle bodyLarge = TextStyle(
+  color: Color.fromRGBO(45, 0, 79, 1),
+  fontFamily: 'Poppins',
+  fontSize: 16,
+  letterSpacing: 0.5,
+  fontWeight: FontWeight.normal,
+  height: 24 / 16,
+);
+
+TextStyle bodyMedium = TextStyle(
+    color: Color.fromRGBO(45, 0, 79, 1),
+    fontFamily: 'Poppins',
+    fontSize: 14,
+    letterSpacing: 0.25,
+    fontWeight: FontWeight.normal,
+    height: 20 / 14);
+
+TextStyle bodySmall = TextStyle(
+  color: Color.fromRGBO(45, 0, 79, 1),
+  fontFamily: 'Poppins',
+  fontSize: 12,
+  letterSpacing: 0.4,
+  fontWeight: FontWeight.normal,
+  height: 16 / 12,
+);
+
+//? LABEL LARGE
+
+TextStyle labelLarge = TextStyle(
+  color: Color.fromRGBO(45, 0, 79, 1),
+  fontFamily: 'Poppins',
+  fontSize: 14,
+  letterSpacing: 0.1,
+  fontWeight: FontWeight.w500,
+  height: 20 / 14,
+);
+
+TextStyle labelMedium = TextStyle(
+  color: Color.fromRGBO(45, 0, 79, 1),
+  fontFamily: 'Poppins',
+  fontSize: 12,
+  letterSpacing: 0.5,
+  fontWeight: FontWeight.w500,
+  height: 16 / 12,
+);
+
+TextStyle labelSmall = TextStyle(
+  color: Color.fromRGBO(45, 0, 79, 1),
+  fontFamily: 'Poppins',
+  fontSize: 11,
+  letterSpacing: 0.5,
+  fontWeight: FontWeight.w500,
+  height: 16 / 11,
+);
+
+//! old typo
+
 TextStyle body12Bold700 = TextStyle(
   color: Colors.white,
   fontSize: 12,
