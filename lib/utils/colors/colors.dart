02975a8eff38312cb15ecc4cf197ed66997135a1
goutham srikar <srikarsam2000@gmail.com>
2022-07-14 23:49:13 +0530

    naan 2.0 colors

--- lib/utils/colors/colors.dart
+++ lib/utils/colors/colors.dart
@@ -66,3 +66,78 @@ final LinearGradient appleBlack = LinearGradient(
 final Color grey = Color(0xffA4A3A9);
 final Color blue = Color(0xff3F9AF7);
 final Color textGrey1 = Color(0xff8D8D8D);
+
+final Color x = Colors.amberAccent;
+
+class GradientConst {
+  GradientConst._();
+
+  final LinearGradient GradientBackground = LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [
+      Color(0xff07030C),
+      Color(0xff2D004F),
+    ],
+  );
+}
+
+class ColorConst {
+  ColorConst._();
+  static const int _Primary = 0xFFF44336;
+  static const int _Secondary = 0xFFF44336;
+  static const int _Tertiary = 0xFFF44336;
+  static const int _Error = 0xFFF44336;
+  static const int _Neutral = 0xFFF44336;
+  static const int _NeutralVariant = 0xFFF44336;
+  static const int _Orange = 0xFFF44336;
+  static const int _NaanRed = 0xFFF44336;
+
+  static const MaterialColor Primary = MaterialColor(
+    _Primary,
+    <int, Color>{
+      0: Color(0xFF000000),
+      10: Color(0xFF280056),
+      20: Color(0xFF440088),
+      30: Color(0xFF6100BE),
+      40: Color(0xFF7C29E1),
+      50: Color(0xFF964AFB),
+      60: Color(0xFFAC72FF),
+      70: Color(0xFFC197FF),
+      80: Color(0xFFD7BAFF),
+      90: Color(0xFFEDDCFF),
+      95: Color(0xFFF8EDFF),
+      99: Color(0xFFFFFBFF),
+      100: Color(0xFFFFFFFF),
+    },
+  );
+
+  static const MaterialColor Secondary = MaterialColor(
+    _Secondary,
+    <int, Color>{},
+  );
+  static const MaterialColor Tertiary = MaterialColor(
+    _Tertiary,
+    <int, Color>{},
+  );
+  static const MaterialColor Error = MaterialColor(
+    _Error,
+    <int, Color>{},
+  );
+  static const MaterialColor Neutral = MaterialColor(
+    _Neutral,
+    <int, Color>{},
+  );
+  static const MaterialColor NeutralVariant = MaterialColor(
+    _NeutralVariant,
+    <int, Color>{},
+  );
+  static const MaterialColor Orange = MaterialColor(
+    _Orange,
+    <int, Color>{},
+  );
+  static const MaterialColor NaanRed = MaterialColor(
+    _NaanRed,
+    <int, Color>{},
+  );
+}
