2b7294fa0bb783cee603ea04054aa0375eb1eb1e
Harsh Raj Singh <harsh@tezsure.com>
2022-12-05 14:46:12 +0530

    feat: edit and remove contact

--- lib/app/modules/send_page/views/widgets/edit_contact_sheet.dart
+++ lib/app/modules/send_page/views/widgets/edit_contact_sheet.dart
@@ -1,57 +1,57 @@
-import 'package:flutter/material.dart';
-import 'package:naan_wallet/app/data/services/service_models/contact_model.dart';
-import 'package:naan_wallet/app/data/services/user_storage_service/user_storage_service.dart';
-import 'package:naan_wallet/app/modules/common_widgets/bottom_sheet.dart';
-import 'package:naan_wallet/app/modules/common_widgets/naan_textfield.dart';
-import 'package:naan_wallet/utils/colors/colors.dart';
-import 'package:naan_wallet/utils/extensions/size_extension.dart';
-import 'package:naan_wallet/utils/styles/styles.dart';
+// import 'package:flutter/material.dart';
+// import 'package:naan_wallet/app/data/services/service_models/contact_model.dart';
+// import 'package:naan_wallet/app/data/services/user_storage_service/user_storage_service.dart';
+// import 'package:naan_wallet/app/modules/common_widgets/bottom_sheet.dart';
+// import 'package:naan_wallet/app/modules/common_widgets/naan_textfield.dart';
+// import 'package:naan_wallet/utils/colors/colors.dart';
+// import 'package:naan_wallet/utils/extensions/size_extension.dart';
+// import 'package:naan_wallet/utils/styles/styles.dart';
 
-class EditContactBottomSheet extends StatelessWidget {
-  final ContactModel contactModel;
-  const EditContactBottomSheet({
-    super.key,
-    required this.contactModel,
-  });
+// class EditContactBottomSheet extends StatelessWidget {
+//   final ContactModel contactModel;
+//   const EditContactBottomSheet({
+//     super.key,
+//     required this.contactModel,
+//   });
 
-  @override
-  Widget build(BuildContext context) {
-    return NaanBottomSheet(
-      height: 262,
-      bottomSheetHorizontalPadding: 32,
-      blurRadius: 5,
-      bottomSheetWidgets: [
-        Center(
-          child: Text(
-            'Edit Contact',
-            style: titleMedium,
-          ),
-        ),
-        0.03.vspace,
-        NaanTextfield(
-          hint: 'Enter Name',
-        ),
-        0.025.vspace,
-        Text(
-          contactModel.address,
-          style: labelSmall.apply(color: ColorConst.NeutralVariant.shade60),
-        ),
-        0.025.vspace,
-        MaterialButton(
-          color: ColorConst.Primary,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-          onPressed: () async {},
-          child: SizedBox(
-            width: double.infinity,
-            height: 48,
-            child: Center(
-                child: Text(
-              'Save Changes',
-              style: titleSmall,
-            )),
-          ),
-        ),
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return NaanBottomSheet(
+//       height: 262,
+//       bottomSheetHorizontalPadding: 32,
+//       blurRadius: 5,
+//       bottomSheetWidgets: [
+//         Center(
+//           child: Text(
+//             'Edit Contact',
+//             style: titleMedium,
+//           ),
+//         ),
+//         0.03.vspace,
+//         NaanTextfield(
+//           hint: 'Enter Name',
+//         ),
+//         0.025.vspace,
+//         Text(
+//           contactModel.address,
+//           style: labelSmall.apply(color: ColorConst.NeutralVariant.shade60),
+//         ),
+//         0.025.vspace,
+//         MaterialButton(
+//           color: ColorConst.Primary,
+//           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+//           onPressed: () async {},
+//           child: SizedBox(
+//             width: double.infinity,
+//             height: 48,
+//             child: Center(
+//                 child: Text(
+//               'Save Changes',
+//               style: titleSmall,
+//             )),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
