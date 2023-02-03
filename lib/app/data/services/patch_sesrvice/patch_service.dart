076cb34f91f67f057b48ecfc581b475afe2621e0
Aniket sindhu <sindhuaniket@gmail.com>
2023-02-03 20:30:38 +0530

    fix: list reading from storage

--- lib/app/data/services/patch_sesrvice/patch_service.dart
+++ lib/app/data/services/patch_sesrvice/patch_service.dart
@@ -5,6 +5,8 @@ import 'package:get/get.dart';
 import 'package:naan_wallet/app/data/services/enums/enums.dart';
 import 'package:naan_wallet/app/data/services/service_config/service_config.dart';
 import 'package:naan_wallet/app/data/services/service_models/account_model.dart';
+import 'package:naan_wallet/app/data/services/user_storage_service/user_storage_service.dart';
+import 'package:naan_wallet/app/data/services/wallet_service/wallet_service.dart';
 import 'package:naan_wallet/app/modules/home_page/controllers/home_page_controller.dart';
 
 class PatchService {
@@ -41,10 +43,10 @@ class PatchService {
                   )));
       }
       if (accountList.isNotEmpty) {
-        List<String?> tz1Addresses = Get.find<HomePageController>()
-            .userAccounts
-            .map((e) => e.publicKeyHash)
-            .toList();
+        List<String?> tz1Addresses =
+            (await UserStorageService().getAllAccount())
+                .map((e) => e.publicKeyHash)
+                .toList();
 
         accountList.removeWhere(
             (element) => tz1Addresses.contains(element.publicKeyHash));
