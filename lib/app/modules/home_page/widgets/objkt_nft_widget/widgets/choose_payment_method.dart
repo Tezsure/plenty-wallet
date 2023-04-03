340f8b8d84a719104e87c9e9b9d74d234292d377
Harsh Raj Singh <harsh@tezsure.com>
2023-04-03 17:35:58 +0530

    fix: 5 dollar limit to open wert

--- lib/app/modules/home_page/widgets/objkt_nft_widget/widgets/choose_payment_method.dart
+++ lib/app/modules/home_page/widgets/objkt_nft_widget/widgets/choose_payment_method.dart
@@ -26,52 +26,82 @@ class ChoosePaymentMethod extends StatelessWidget {
     final buyNftController = Get.put(BuyNFTController());
     return SizedBox(
       // height: 0.7.height,
-      child: NaanBottomSheet(
-        // height: 0.7.height,
-        isScrollControlled: true,
-        title: "Choose payment",
-        // bottomSheetHorizontalPadding: 16.arP,
-        bottomSheetWidgets: [
-          0.012.vspace,
-          NaanListTile(
-            onTap: buyNftController.buyWithCreditCard,
-            dense: true,
-            contentPadding: EdgeInsets.zero,
-            leading: SvgPicture.asset("assets/svg/credit-card.svg"),
-            title: Text(
-              "Credit card".tr,
-              style: labelLarge,
+      child: Obx(() {
+        return NaanBottomSheet(
+          // height: 0.7.height,
+          isScrollControlled: true,
+          title: "Choose payment",
+          // bottomSheetHorizontalPadding: 16.arP,
+          bottomSheetWidgets: [
+            0.012.vspace,
+            _buildBuyWithCreditCard(buyNftController, accountSummaryController),
+            const Divider(
+              color: ColorConst.darkGrey,
+              thickness: 1,
             ),
-            subtitle: Text(
-              "${"Powered by".tr} wert.io",
-              style: bodySmall.copyWith(color: ColorConst.textGrey1),
+            Obx(
+              () => buyNftController.accountTokens.isEmpty
+                  ? Center(child: noTokens())
+                  : ListView.builder(
+                      itemCount: buyNftController.accountTokens.length,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return Obx(
+                          () => _tokenBox(
+                            accountSummaryController.xtzPrice.value,
+                            buyNftController.accountTokens[index],
+                            index,
+                            buyNftController,
+                          ),
+                        );
+                      },
+                    ),
             ),
-          ),
-          const Divider(
-            color: ColorConst.darkGrey,
-            thickness: 1,
-          ),
-          Obx(
-            () => buyNftController.accountTokens.isEmpty
-                ? Center(child: noTokens())
-                : ListView.builder(
-                    itemCount: buyNftController.accountTokens.length,
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return Obx(
-                        () => _tokenBox(
-                          accountSummaryController.xtzPrice.value,
-                          buyNftController.accountTokens[index],
-                          index,
-                          buyNftController,
-                        ),
-                      );
-                    },
-                  ),
-          ),
-          BottomButtonPadding()
-        ],
+            BottomButtonPadding()
+          ],
+        );
+      }),
+    );
+  }
+
+  Widget _buildBuyWithCreditCard(BuyNFTController buyNftController,
+      AccountSummaryController accountSummaryController) {
+    if (buyNftController.selectedNFT.value == null) return Container();
+    double priceInDollars =
+        (double.parse(buyNftController.selectedNFT.value!.lowestAsk) / 1e6) *
+            accountSummaryController.xtzPrice.value;
+    if (priceInDollars < 5) {
+      return NaanListTile(
+        // onTap: buyNftController.buyWithCreditCard,
+        // dense: true,
+        contentPadding: EdgeInsets.zero,
+        leading: SvgPicture.asset(
+          "assets/svg/credit-card.svg",
+          color: ColorConst.darkGrey,
+        ),
+        title: Text(
+          "Credit card".tr,
+          style: labelLarge.copyWith(color: ColorConst.NeutralVariant.shade60),
+        ),
+        subtitle: Text(
+          "Credit card can’t be used for transactions less than \$5. You can purchase tez using credit card from “Buy Tez” widget on home page and come back here to buy the NFT.",
+          style: bodySmall.copyWith(color: ColorConst.textGrey1),
+        ),
+      );
+    }
+    return NaanListTile(
+      onTap: buyNftController.buyWithCreditCard,
+      dense: true,
+      contentPadding: EdgeInsets.zero,
+      leading: SvgPicture.asset("assets/svg/credit-card.svg"),
+      title: Text(
+        "Credit card".tr,
+        style: labelLarge,
+      ),
+      subtitle: Text(
+        "${"Powered by".tr} wert.io",
+        style: bodySmall.copyWith(color: ColorConst.textGrey1),
       ),
     );
   }
