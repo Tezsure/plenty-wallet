561cc8cc5b4aa3e010aa05b6152db214cb9dcd80
Jay Daraniya <40439741+JAYDARANIYA@users.noreply.github.com>
2024-03-22 08:42:30 +0530

    refactor: Remove unused import 'firebase_analytics'

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:developer';
 import 'dart:ui';
 
-// import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:dartez/dartez.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
